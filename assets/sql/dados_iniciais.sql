9f01e6091e0db7d86d11ef8967d22b3ec6029a73
ESLL3N <esllenescolar@gmail.com>
2021-09-16 17:30:07 +0000

    alteração final bd

--- assets/sql/dados_iniciais.sql
+++ assets/sql/dados_iniciais.sql
@@ -5,7 +5,7 @@ insert into generos (id, nome) values
 (4, 'Ficção Científica');
 
 insert into livros(id, titulo, id_genero) values
-    (1, 'Assassinato no expresso do Oriente', 2)
-    (2, 'A sociedade do Anel,' 1)
-    (3, 'Como era eu antes de você', 3)
-    (4, 'O guia do mochileiro das Galáxias', 4)
+    (1, 'Assassinato no expresso do Oriente', 2),
+    (2, 'A sociedade do Anel', 1),
+    (3, 'Como era eu antes de você', 3),
+    (4, 'O guia do mochileiro das Galáxias', 4);
